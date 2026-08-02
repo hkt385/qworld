b69d86a08497c0e5f6248e7469da64c69ee6177d
hkt385 <hiranmayikoduritirumala@gmail.com>
2026-08-02 13:31:59 +0530

    Updates

--- src/scenes/Quiz15.ts
+++ src/scenes/Quiz15.ts
@@ -8,64 +8,6 @@ export default class Quiz15 extends BaseQuiz {
 
     protected questions = [
 
-        {
-            question: "Which field can use quantum computing to speed up drug discovery?",
-            options: [
-                "Healthcare",
-                "Construction",
-                "Sports",
-                "Gaming"
-            ],
-            answer: 0
-        },
-
-        {
-            question: "Quantum Machine Learning combines quantum computing with...",
-            options: [
-                "Agriculture",
-                "Artificial Intelligence",
-                "Architecture",
-                "Astronomy"
-            ],
-            answer: 1
-        },
-
-        {
-            question: "Which industry can benefit from quantum optimization?",
-            options: [
-                "Finance",
-                "Fashion",
-                "Photography",
-                "Music"
-            ],
-            answer: 0
-        },
-
-        {
-            question: "Which of these is a potential application of quantum computing?",
-            options: [
-                "Traffic Routing",
-                "Supply Chain Optimization",
-                "Scientific Simulation",
-                "All of the above"
-            ],
-            answer: 3
-        },
-
-        {
-            question: "Why are companies investing in quantum computing?",
-            options: [
-                "To solve complex problems faster",
-                "To replace the internet",
-                "To build larger keyboards",
-                "To increase screen brightness"
-            ],
-            answer: 0
-        }
-
-    ];
-protected questions = [
-
     {
         question: "What mathematical structure is used in many post-quantum cryptography algorithms?",
         options: [
@@ -122,6 +64,7 @@ protected questions = [
     }
 
 ];
+
     constructor() {
         super("Quiz15");
     }
